dcdf105d9eee6ae1bd7a284feb1718649d62051d
sencerarslan <m.sencerarslan@gmail.com>
2023-11-01 08:59:34 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React, {useState} from 'react';
-import VideoPlayer from './components/VideoPlayer'; 
-
+import React, { useState } from "react";
+import VideoPlayerPage from "./components/VideoPlayer";
+import "./App.css";
 
 function App() {
-  const [videoId, setVideoId] = useState('YyOASMrGQzQ'); 
-  const [videoIdKey, setVideoIdKey] = useState(videoId); 
+  const [videoId, setVideoId] = useState("https://www.youtube.com/watch?v=YyOASMrGQzQ");
+  const [videoIdKey, setVideoIdKey] = useState(videoId);
+
+  function extractYouTubeVideoId(url) {
+    // YouTube video ID'sini çıkarmak için düzenli ifade
+    const regex = /[?&]v=([^&]+)/;
+    const match = url.match(regex);
+
+    // Eğer eşleşme varsa video ID'sini döndür, yoksa null döndür
+    return match ? match[1] : null;
+  }
+
   const handleSubmit = (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
+
     setVideoIdKey(videoId);
   };
   return (
-    <div className="App"> 
-   <form onSubmit={handleSubmit}>
-   <input style={{boxSizing:'border-box', width:'100%', padding:'10px'}} value={videoId} placeholder='Youtube Video ID' onChange={((e) => {
-      setVideoId(e.target.value)
-    })} />
-   </form>
-      <VideoPlayer key={videoIdKey} videoId={videoIdKey}  />
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          style={{ boxSizing: "border-box", width: "100%", padding: "10px" }}
+          value={videoId}
+          placeholder="Youtube URL"
+          onChange={(e) => {
+            setVideoId(e.target.value);
+          }}
+        />
+        <button type="submit">Ara...</button>
+      </form>
+      <div className="video">
+        <VideoPlayerPage key={videoIdKey} videoId={extractYouTubeVideoId(videoIdKey)} />
+      </div>
     </div>
   );
 }
--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,20 +1,51 @@
-import React from 'react';
-const subtitleUrlRU = '/assets/ru.vtt';
-const subtitleUrlTR = '/assets/tr.vtt';
-const subtitleUrlEN = '/assets/en.vtt';
+import React from "react";
+import VideoPlayer from "./video.tsx";
+const subtitleUrlRU = "/assets/ru.vtt";
+const subtitleUrlTR = "/assets/tr.vtt";
+const subtitleUrlEN = "/assets/en.vtt";
 
-const VideoPlayer = ({ videoId }) => {
-    const videoUrl = `https://invidious.fdn.fr/latest_version?id=${videoId}&itag=22`; 
-    return (
-        <div>
-            <video autoPlay width="100%" height="auto" controls poster={`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`}>
-            <source src={videoUrl} type="video/mp4" />
-                <track src={subtitleUrlEN} kind="subtitles" srclang="en" label="İngilizce"/> 
-                <track src={subtitleUrlTR} kind="subtitles" srclang="tr" label="Türkçe" default /> 
-                <track src={subtitleUrlRU} kind="subtitles" srclang="ru" label="Rusça"/> 
-            </video> 
-        </div>
-    );
+const VideoPlayerPage = ({ videoId }) => {
+  const videoUrl = `https://invidious.fdn.fr/latest_version?id=${videoId}&itag=22`;
+
+  const videoJsOptions = {
+    sources: [
+      {
+        src: videoUrl,
+        type: "video/mp4",
+      },
+    ],
+    autoplay: true,
+    poster: `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`,
+    tracks: [
+      {
+        src: subtitleUrlEN,
+        kind: "subtitles",
+        srclang: "en",
+        label: "İngilizce",
+        default: false,
+      },
+      {
+        src: subtitleUrlTR,
+        kind: "subtitles",
+        srclang: "tr",
+        label: "Türkçe",
+        default: true,
+      },
+      {
+        src: subtitleUrlRU,
+        kind: "subtitles",
+        srclang: "ru",
+        label: "Rusça",
+        default: false,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <VideoPlayer options={videoJsOptions} />
+    </div>
+  );
 };
 
-export default VideoPlayer;
+export default VideoPlayerPage;
